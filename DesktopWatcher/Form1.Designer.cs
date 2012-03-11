a09f6c1a6d167cfc20d8fdb5515e75ecaa8cbe69
Piotr Benetkiewicz <piotr@ais.pl>
2012-03-11 13:16:50 +0100

    initial commit to local repo

---
+++ DesktopWatcher/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DesktopWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.TrayIcon.BalloonTipText = "There is a new file on desktop";
+            this.TrayIcon.BalloonTipTitle = "New file on desktop";
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "Desktop Watcher";
+            this.TrayIcon.Visible = true;
+            this.TrayIcon.BalloonTipClicked += new System.EventHandler(this.TrayIconBalloonTipClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormUnloadHandler);
+            this.Load += new System.EventHandler(this.FormLoadHandler);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+    }
+}
+
---
+++ DesktopWatcher/Form1.cs
@@ -0,0 +1,57 @@
+﻿namespace DesktopWatcher
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        private FileSystemWatcher desktopWatcher;
+        private string fileName;
+        const string DesktopPath = @"c:\Users\piotr\Desktop\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the form load.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        public void FormLoadHandler(object sender, EventArgs eventArgs)
+        {
+            desktopWatcher = new FileSystemWatcher();
+            desktopWatcher.Path = DesktopPath;
+            desktopWatcher.Filter = "*.*";
+            desktopWatcher.NotifyFilter = NotifyFilters.FileName;
+            desktopWatcher.Created += FileOnDesktopCreated;
+            desktopWatcher.EnableRaisingEvents = true;
+        }
+
+        public void FormUnloadHandler(object sender, FormClosingEventArgs e)
+        {
+            if (this.desktopWatcher != null)
+            {
+                desktopWatcher.EnableRaisingEvents = false;
+                desktopWatcher.Dispose();
+            }
+        }
+
+        void FileOnDesktopCreated(object sender, FileSystemEventArgs e)
+        {
+            fileName = e.Name;
+            TrayIcon.BalloonTipText = string.Format("File {0} appeared on desktop!", e.Name);
+            TrayIcon.ShowBalloonTip(7*1000);
+        }
+
+        private void TrayIconBalloonTipClicked(object sender, EventArgs e)
+        {
+            string args = string.Format("/Select, {0}{1}", DesktopPath, fileName);
+            var processStartInfo = new ProcessStartInfo("Explorer.exe", args);
+            Process.Start(processStartInfo);
+        }
+    }
+}
